ea191b66821d4b526a80170f0ce9e1aa84195ed1
Rohit0301 <rj03012002@gmail.com>
2020-12-17 01:19:05 +0530

    login view serializer changed

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.generics import GenericAPIView
-from .serializers import UserSerializer
+from .serializers import UserSerializer,LoginSerializer
 from rest_framework.response import Response
 from rest_framework import serializers
 from rest_framework import status
@@ -21,7 +21,7 @@ class RegisterView(GenericAPIView):
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
 class LoginView(GenericAPIView):
-    serializer_class=UserSerializer
+    serializer_class=LoginSerializer
     def post(self,request):
         data=request.data
         username=data.get('username','')
