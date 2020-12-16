955e30bee30d619219618d2e227849a55a768477
Rohit0301 <rj03012002@gmail.com>
2020-12-17 01:18:21 +0530

    login serializer added

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -21,3 +21,11 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self,validated_data):
         return User.objects.create_user(**validated_data)
+
+class LoginSerializer(serializers.ModelSerializer):
+    password=serializers.CharField(max_length=6,min_length=8,write_only=True)
+    username=serializers.CharField(max_length=55,min_length=2)
+
+    class Meta:
+        model=User
+        fields=['username','password']
